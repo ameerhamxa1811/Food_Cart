76a5399938bf5b5fc80e90a7cc7192519a763faa
Ameer Hamza <ameerhamxa1811@gmail.com>
2024-12-25 18:03:26 +0500

    changes

--- android/app/src/main/kotlin/com/example/food_cart/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/food_cart/MainActivity.kt
@@ -2,4 +2,6 @@ package com.example.food_cart
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity()
